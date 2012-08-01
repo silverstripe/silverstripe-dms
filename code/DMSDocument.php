99435e39b71cee56adb0386eb8d3f7bfabe9ae71
Normann Lou <normannlou@gmail.com>
2012-08-01 13:23:20 +1200

    BUGFIX: use the new API method getFilenameWithoutID()

--- code/DMSDocument.php
+++ code/DMSDocument.php
@@ -596,7 +596,7 @@ class DMSDocument_Controller extends Controller {
 					}
 					header('Content-Type: ' . $mime);
 					header('Content-Length: ' . filesize($path), null);
-					if (!empty($mime) && $mime != "text/html") header('Content-Disposition: attachment; filename="'.$doc->filenameWithoutID().'"');
+					if (!empty($mime) && $mime != "text/html") header('Content-Disposition: attachment; filename="'.$doc->getFilenameWithoutID().'"');
 					header('Content-transfer-encoding: 8bit');
 					header('Expires: 0');
 					header('Pragma: cache');
