fd94ad19f456dfd9a7f9b3a7a8283f319bbc3c2d
Julian Seidenberg <julian@silverstripe.com>
2012-08-06 18:14:34 +1200

    MINOR: jslint suggested improvements

--- javascript/DMSGridField.js
+++ javascript/DMSGridField.js
@@ -1,7 +1,7 @@
 (function($){
+	"use strict";
 
 	$.entwine('ss', function($) {
-
 
 		$('.ss-gridfield .action.dms-delete').entwine({
 			onclick: function(e){
@@ -9,12 +9,18 @@
 				var pagesCount = this.data('pages-count');
 				var pagesCountAfterDeletion = pagesCount - 1;
 				var addS = 's';
-				if (pagesCountAfterDeletion == 1) addS = '';
+				if (pagesCountAfterDeletion === 1) {
+					addS = '';
+				}
 
 				//display an appropriate message
 				var message = '';
-				if (this.hasClass('dms-delete-last-warning')) message = "Permanently delete this document?\n\nWarning: this document is attached only to this page, deleting it here will delete it permanently.";
-				if (this.hasClass('dms-delete-link-only')) message = "Unlink this document from this page?\n\nNote: it will remain attached to "+pagesCountAfterDeletion+" other page"+addS+".";
+				if (this.hasClass('dms-delete-last-warning')) {
+					message = "Permanently delete this document?\n\nWarning: this document is attached only to this page, deleting it here will delete it permanently.";
+				}
+				if (this.hasClass('dms-delete-link-only')) {
+					message = "Unlink this document from this page?\n\nNote: it will remain attached to "+pagesCountAfterDeletion+" other page"+addS+".";
+				}
 
 				if(!confirm(message)) {
 					e.preventDefault();
@@ -25,7 +31,6 @@
 				}
 			}
 		});
-
 
 	});
 
