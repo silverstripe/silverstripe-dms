782b07ec8002196920c53252ae817451f0b3ed07
Ryan O'Hara <ryan@silverstripe.com>
2012-08-06 17:55:04 +1200

    MINOR: Adjust styling for gridfield showing where-else document is linked

--- code/DMSDocument.php
+++ code/DMSDocument.php
@@ -472,7 +472,7 @@ class DMSDocument extends DataObject implements DMSDocumentInterface {
 		$fields->add($UploadField);
 		$fields->add($pagesGrid);
 		$fields->add(FormAction::create('dod', _t('GridFieldDetailForm.Delete', 'Delete'))
-						->addExtraClass('ss-ui-action-destructive'));   //delete button
+						->addExtraClass('ss-ui-action-destructive'));   //delete button - move this to actions area above
 
 
 		return $fields;
