1b17a478c96c7a1d514d0ffa8c0ca95c8b1669f6
Normann Lou <normannlou@gmail.com>
2012-09-21 13:03:53 +1200

    BUGFIX: since we change the option value for 'download a document' option in InsertLinkForm, as 'documentopt', we need to use the same string for our js code

--- javascript/DocumentHtmlEditorFieldToolbar.js
+++ javascript/DocumentHtmlEditorFieldToolbar.js
@@ -10,7 +10,7 @@
 				var form = $('form.htmleditorfield-linkform');
 				var show = false;
 
-				if (this.attr('value') === 'document') {
+				if (this.attr('value') === 'documentopt') {
 					if (this.is(':checked')) {
 						show = true;
 					}
