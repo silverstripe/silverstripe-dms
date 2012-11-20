923a21376d0099e7597c52d62dd0a1f0e1291608
Julian Seidenberg <julian@silverstripe.com>
2012-11-21 10:43:31 +1300

    ENHANCEMENT: adding a check for dms directory name

--- _config.php
+++ _config.php
@@ -6,3 +6,5 @@ Object::add_extension('HtmlEditorField_Toolbar','DocumentHtmlEditorFieldToolbar'
 CMSMenu::remove_menu_item('DMSDocumentAddController');
 
 ShortcodeParser::get('default')->register('dms_document_link', array('DMSDocument_Controller', 'dms_link_shortcode_handler'));
+
+if (!file_exists(BASE_PATH . DIRECTORY_SEPARATOR . DMS_DIR)) user_error("DMS directory named incorrectly. Please install the DMS module into a folder named: ".DMS_DIR);
