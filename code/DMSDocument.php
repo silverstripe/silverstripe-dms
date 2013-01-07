silverstripe/silverstripe-dms

commit 18c2081be73e44e3cc5d48ed619bf76ec826a750

ENHANCEMENT: deleting the old file when replacing, if document versioning is turned off

code/DMSDocument.php
@@ -444,6 +444,9 @@ class DMSDocument extends DataObject implements DMSDocumentInterface {
 		//version the existing file (copy it to a new "very specific" filename
 		if (DMSDocument_versions::$enable_versions) {
 			DMSDocument_versions::create_version($this);
+		} else {    //otherwise delete the old document file
+			$oldPath = $this->getFullPath();
+			if (file_exists($oldPath)) unlink($this->getFullPath());
 		}
 
 		copy($fromPath, $toPath);   //this will overwrite the existing file (if present)

commit 6b7ee0623aad28257eb673294e9830a5070b3b8e

Only return DMSDocument->FullPath() when filename exists
Fixes a regression introduced with 18c2081b when uploading new files

code/DMSDocument.php
@@ -374,7 +374,11 @@ class DMSDocument extends DataObject implements DMSDocumentInterface {
 	 * @return string
 	 */
 	function getFullPath() {
-		return DMS::get_dms_path() . DIRECTORY_SEPARATOR . $this->Folder . DIRECTORY_SEPARATOR . $this->Filename;
+		if($this->Filename) {
+			return DMS::get_dms_path() . DIRECTORY_SEPARATOR . $this->Folder . DIRECTORY_SEPARATOR . $this->Filename;	
+		} else {
+			return false;
+		}
 	}
 
 	/**
@@ -446,7 +450,7 @@ class DMSDocument extends DataObject implements DMSDocumentInterface {
 			DMSDocument_versions::create_version($this);
 		} else {    //otherwise delete the old document file
 			$oldPath = $this->getFullPath();
-			if (file_exists($oldPath)) unlink($this->getFullPath());
+			if (file_exists($oldPath)) unlink($oldPath);
 		}
 
 		copy($fromPath, $toPath);   //this will overwrite the existing file (if present)