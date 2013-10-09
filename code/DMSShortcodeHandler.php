76c4692d13162e11cab47f526f5688931c001a82
Andrew Short <andrewjshort@gmail.com>
2013-10-09 20:54:08 +1100

    NEW: Add metadata to DMS links.
    Shortlink links are now augmented with data attributes containing the
    file extension and size.

--- code/DMSShortcodeHandler.php
+++ code/DMSShortcodeHandler.php
@@ -5,28 +5,35 @@
  */
 class DMSShortcodeHandler {
 
-	public static function handle($arguments, $content = null, $parser = null) {
-		$linkText = null;
+	public static function handle(
+		$arguments, $content, ShortcodeParser $parser, $tag, array $extra = array()
+	) {
+		if(!empty($arguments['id'])) {
+			$document = DMSDocument::get()->byID($arguments['id']);
 
-		if (!empty($arguments['id'])) {
-			$document = DMSDocument::get()->filter(array('ID' => $arguments['id']))->First();
-			if ($document && !$document->isHidden()) {
-				if (!empty($content)) {
-					$linkText = sprintf('<a href="%s">%s</a>', $document->Link(), $parser->parse($content));
+			if($document && !$document->isHidden()) {
+				if($content) {
+					return sprintf(
+						'<a href="%s">%s</a>', $document->Link(), $parser->parse($content)
+					);
 				} else {
-					$linkText = $document->Link();
+					if(isset($extra['element'])) {
+						$extra['element']->setAttribute('data-ext', $document->getExtension());
+						$extra['element']->setAttribute('data-size', $document->getFileSizeFormatted());
+					}
+
+					return $document->Link();
 				}
 			}
 		}
 
-		if (empty($linkText)) {
-			$errorPage = ErrorPage::get()->filter(array('ErrorCode' => '404'))->First();
-			if ($errorPage) {
-				$linkText = $errorPage->Link();
-			}
+		$error = ErrorPage::get()->filter('ErrorCode', '404')->First();
+
+		if($error) {
+			return $error->Link();
 		}
 
-		return $linkText;
+		return '';
 	}
 
 }
