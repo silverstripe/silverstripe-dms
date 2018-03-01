2c204bb0b65fe78a0d18fec0cce49360964a22ae
Florian Thoma <florian.thoma@innoweb.com.au>
2018-03-01 12:15:44 +1100

    remove DMSSiteTreeExtension.getDocumentSets() because it messes with the page history (readonly mode)

--- code/extensions/DMSSiteTreeExtension.php
+++ code/extensions/DMSSiteTreeExtension.php
@@ -53,16 +53,6 @@ class DMSSiteTreeExtension extends DataExtension
     }
 
     /**
-     * Get a list of document sets for the owner page
-     *
-     * @return ArrayList
-     */
-    public function getDocumentSets()
-    {
-        return $this->owner->DocumentSets();
-    }
-
-    /**
      * Get a list of all documents from all document sets for the owner page
      *
      * @return ArrayList
@@ -71,7 +61,7 @@ class DMSSiteTreeExtension extends DataExtension
     {
         $documents = ArrayList::create();
 
-        foreach ($this->getDocumentSets() as $documentSet) {
+        foreach ($this->owner->DocumentSets() as $documentSet) {
             /** @var DocumentSet $documentSet */
             $documents->merge($documentSet->getDocuments());
         }
