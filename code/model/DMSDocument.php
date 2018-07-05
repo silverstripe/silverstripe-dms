71c09244f7b554ad68bd12dbf0d09da19fadb7ae
Dave Collins <dave.collins@innismaggiore.com>
2018-07-05 11:58:18 -0400

    Make sure default DownloadBehavior is stored when initially uploading a document

--- code/model/DMSDocument.php
+++ code/model/DMSDocument.php
@@ -235,7 +235,7 @@ class DMSDocument extends DataObject implements DMSDocumentInterface
             $this->ViewCount = $count;
 
             DB::query("UPDATE \"DMSDocument\" SET \"ViewCount\"='$count' WHERE \"ID\"={$this->ID}");
-            
+
             $this->extend('trackView');
         }
 
@@ -1002,6 +1002,18 @@ class DMSDocument extends DataObject implements DMSDocumentInterface
                 $this->CreatedByID = $currentUserID;
             }
             $this->LastEditedByID = $currentUserID;
+        }
+
+        // make sure default DownloadBehavior is respected when writing since
+        $defaultDownloadBehaviour = Config::inst()->get('DMSDocument', 'default_download_behaviour');
+        if ($this->DownloadBehavior == null && !empty($defaultDownloadBehaviour)) {
+            $possibleBehaviors = $this->dbObject('DownloadBehavior')
+                ->enumValues();
+
+            $behavior =$possibleBehaviors[$defaultDownloadBehaviour];
+            if ($behavior) {
+                $this->DownloadBehavior = $behavior;
+            }
         }
     }
 
