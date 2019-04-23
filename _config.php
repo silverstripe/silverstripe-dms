f3f27d26cccfd63760b798d3e2d3d550555dbdd1
thiemen <nt@sunnysideup.co.nz>
2019-04-23 18:39:49 +1200

    PATCH: fixes

--- _config.php
+++ _config.php
@@ -21,7 +21,7 @@ if (!file_exists(Director::baseFolder() . DIRECTORY_SEPARATOR . DMS_DIR)) {
 
 // Ensure compatibility with PHP 7.2 ("object" is a reserved word),
 // with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
-if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+// if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
 
 CMSMenu::remove_menu_item(DMSDocumentAddController::class);
 
@@ -32,11 +32,11 @@ ShortcodeParser::get('default')->register(
 
 if ($config->get(DMSDocument_versions::class, 'enable_versions')) {
     //using the same db relations for the versioned documents, as for the actual documents
-    $config->update(DMSDocument_versions::class, 'db', $config->get(DMSDocument::class, 'db'));
+    // Config::modify()->update(DMSDocument_versions::class, 'db', $config->get(DMSDocument::class, 'db'));
 }
 
 // add dmsassets folder to file system sync exclusion
 if (strpos($config->get(DMS::class, 'folder_name'), 'assets/') === 0) {
     $folderName = substr($config->get(DMS::class, 'folder_name'), 7);
-    $config->update(Filesystem::class, 'sync_blacklisted_patterns', array("/^" . $folderName . "$/i",));
+    // Config::modify()->update(Filesystem::class, 'sync_blacklisted_patterns', array("/^" . $folderName . "$/i",));
 }
