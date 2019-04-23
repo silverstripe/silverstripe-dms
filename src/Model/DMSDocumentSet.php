7eab74e1dfc7dba2b4470d210c6fd43f0fe06cf1
thiemen <nt@sunnysideup.co.nz>
2019-04-23 18:29:20 +1200

    MAJOR: core upgrade to SS4: running INSPECT on /var/www/upgrades/upgradeto4/dms

--- src/Model/DMSDocumentSet.php
+++ src/Model/DMSDocumentSet.php
@@ -83,14 +83,14 @@ class DMSDocumentSet extends DataObject
 /**
   * ### @@@@ START REPLACEMENT @@@@ ###
   * OLD: private static $db (case sensitive)
-  * NEW: 
+  * NEW:
     private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
 
     private static $db (COMPLEX)
   * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
   * ### @@@@ STOP REPLACEMENT @@@@ ###
   */
-    
+
     private static $table_name = 'DMSDocumentSet';
 
 
@@ -450,7 +450,7 @@ class DMSDocumentSet extends DataObject
         );
     }
 
-    protected function validate()
+    public function validate()
     {
         $result = parent::validate();
 
