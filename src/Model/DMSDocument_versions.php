78f7ec9307c96ffb5607198d0efe90016acfe24e
thiemen <nt@sunnysideup.co.nz>
2019-04-23 18:34:17 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Apply PSR2 Cleanup.

--- src/Model/DMSDocument_versions.php
+++ src/Model/DMSDocument_versions.php
@@ -35,7 +35,7 @@ class DMSDocument_versions extends DataObject
       * OLD: private static $db (case sensitive)
       * NEW:
         private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
-    
+
         private static $db (COMPLEX)
       * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
       * ### @@@@ STOP REPLACEMENT @@@@ ###
